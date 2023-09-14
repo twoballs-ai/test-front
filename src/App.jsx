5746d134e47f2c356068700aea7bf5cb10a7fc95
twoballs-ai <twoballs20@gmail.com>
2023-09-15 01:54:14 +0300

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import parse from 'html-react-parser';
+
+const serverUrl = "http://127.0.0.1:8000";
 
 function App() {
+  
+const [blockData, setBlockData] = useState([]);
+useEffect(() => {
+  try {
+    axios
+      .get(
+        serverUrl + "/api/blocks/"
+        // ,{ headers: { Authorization: `Token da0d550bcc813a1b1cc6b905551cb11e3bf95046` } }
+        // ,{headers: { "Content-Type": "multipart/form-data" }}
+      )
+      .then((response) => {
+        console.log(response.data);
+        setBlockData(response.data);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+}, []);
   return (
     <section className="Section">
       <div className="Content">
         <div className="Header">
+          <div className="LogoWrapper">
           <div className="Logo">
             <img
               className="LogoImage"
@@ -13,12 +37,29 @@ function App() {
               alt="spaceX"
             />
           </div>
+          </div>
           <Navbar />
         </div>
+        <div className="Moon"></div>
         <div className="Container">
           <div className="Text">
-            <span className="Gradient">ПУТЕШЕСТВИЕ</span><br />
-            <span className="Gradient2">на красную планету</span>
+            <span className="TextUpper">ПУТЕШЕСТВИЕ</span>
+            <br />
+            <span className="TextLower">на красную планету</span>
+          </div>
+          <div className="Button">
+            <span>Начать путешествие</span>
+          </div>
+          <div className="Blocks">
+          {blockData.map((element, index) => {
+              return (
+                <div className={`Block-${element.block_sort}`} key={element.block_sort}><div className="TextBlock" >{parse(element.text_block)}</div></div>
+              );
+            })} 
+            {/* <div className="Block-1"><div className="TextBlock">Мы <br /><span>1</span><br /> на рынке </div></div>
+            <div className="Block-2"><div className="TextBlock">Гарантируем <br /><span>50%</span><br />  безопасность</div></div>
+            <div className="Block-3"><div className="TextBlock">Календарик за <br /> <span>2001</span>г.<br />  в подарок</div></div>
+            <div className="Block-4"><div className="TextBlock">Путешествие за <br /> <span>597</span><br /> дней</div></div> */}
           </div>
         </div>
       </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 import "./navbar.css";
 import { ReactComponent as Hamburger } from "./assets/burger-menu-svgrepo-com.svg";
+
+const serverUrl = "http://127.0.0.1:8000";
+
 const Navbar = () => {
+  const [menuData, setMenuData] = useState([]);
+  useEffect(() => {
+    try {
+      axios
+        .get(
+          serverUrl + "/api/menu/"
+          // ,{ headers: { Authorization: `Token da0d550bcc813a1b1cc6b905551cb11e3bf95046` } }
+          // ,{headers: { "Content-Type": "multipart/form-data" }}
+        )
+        .then((response) => {
+          console.log(response.data);
+          setMenuData(response.data);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
   const [showNavbar, setShowNavbar] = useState(false);
 
   const handleShowNavbar = () => {
@@ -12,28 +33,17 @@ const Navbar = () => {
     <nav className="Navbar">
       <div className="Menu">
         <div className="MenuIcon" onClick={handleShowNavbar}>
-          <Hamburger fill='red' />
+          <Hamburger fill="red" />
         </div>
         <div className={`NavElements ${showNavbar && "active"}`}>
           <ul>
-            <li>
-              <NavLink to="/">Главная</NavLink>
-            </li>
-            <li>
-              <NavLink to="/tech">Технология</NavLink>
-            </li>
-            <li>
-              <NavLink to="/graph">График полетов</NavLink>
-            </li>
-            <li>
-              <NavLink to="/guarant">Гарантии</NavLink>
-            </li>
-            <li>
-              <NavLink to="/about">О компании</NavLink>
-            </li>
-            <li>
-              <NavLink to="/contact">Контакты</NavLink>
-            </li>
+            {menuData.map((element, index) => {
+              return (
+                <li key={element.menu_sort}>
+                  <NavLink to={element.menu_link}>{element.menu_title}</NavLink>
+                </li>
+              );
+            })}
           </ul>
         </div>
       </div>
